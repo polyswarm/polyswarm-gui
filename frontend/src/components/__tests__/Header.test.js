8b4c3cb1284b553b4a9102d35ca1cf184a4b9501
Robert Lathrop <rl@polyswarm.io>
2018-05-01 19:07:20 -0400

    More header tests.

--- frontend/src/components/__tests__/Header.test.js
+++ frontend/src/components/__tests__/Header.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {render} from 'enzyme';
-import {renderToJson} from 'enzyme-to-json';
+import {render, shallow} from 'enzyme';
+import {renderToJson, shallowToJson} from 'enzyme-to-json';
 import Header from '../Header';
 
 it('renders without crashing', () => {
-const wrapper = render(<Header />);
+  const wrapper = render(<Header />);
   expect(renderToJson(wrapper)).toMatchSnapshot();
+});
+
+it('Uses title PolySwarm ', () => {
+  const wrapper = shallow(<Header/>);
+  expect(wrapper.text()).toEqual('PolySwarm Relay');
 });
