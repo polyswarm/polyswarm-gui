89d39c530e45bf4115efa7082cf2212af06b9b97
Robert Lathrop <rl@polyswarm.io>
2018-05-02 20:08:36 -0400

    Add more tests.

--- frontend/src/components/__tests__/Landing.test.js
+++ frontend/src/components/__tests__/Landing.test.js
@@ -36,3 +36,42 @@ it('disables the Go button when address length is 0', () => {
 
   expect(wrapper.find('.Button').props().disabled).toBeTruthy();
 });
+
+it('calls setstate with the address when address modified', () => {
+  const setState = jest.spyOn(Landing.prototype, 'setState');
+  const wrapper = mount(<Landing />);
+
+  wrapper.find('.AddressField').find('input').simulate('change', {target: {value: '012345678912345'}});
+
+  expect(setState).toHaveBeenCalledWith({address: '012345678912345', error: false});
+});
+
+it('calls setstate with the address & error true when address modified with invalid value', () => {
+  const setState = jest.spyOn(Landing.prototype, 'setState');
+  const wrapper = mount(<Landing />);
+
+  wrapper.find('.AddressField').find('input').simulate('change', {target: {value: '012345912345'}});
+
+  expect(setState).toHaveBeenCalledWith({address: '012345912345', error: true});
+});
+
+it('sets hide to true when button is clicked', () => {
+  const setState = jest.spyOn(Landing.prototype, 'setState');
+  const wrapper = mount(<Landing />);
+  wrapper.setState({address: '0123456789123456'});
+  setState.mockClear();
+  wrapper.find('button').simulate('click');
+
+  expect(setState).toHaveBeenCalledWith({hide: true});
+});
+
+it('should call onSetAddress when transition finishes', () => {
+  const onSetAddress = jest.fn();
+  const wrapper = mount(<Landing onSetAddress={onSetAddress} />);
+  wrapper.setState({address: '0123456789123456'});
+
+  const instance = wrapper.find('CSSTransition').first().instance();
+  instance.onExited();
+
+  expect(onSetAddress).toHaveBeenCalledWith('0123456789123456');
+});
