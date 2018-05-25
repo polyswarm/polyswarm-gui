b5bc51bc903b47112c5a8cf4ebb577e8aba490e8
Robert Lathrop <rl@polyswarm.io>
2018-05-25 16:43:34 -0400

    Fix missed guid validation.

--- src/components/OfferPay/http.js
+++ src/components/OfferPay/http.js
@@ -1,10 +1,19 @@
+import validator from 'validator';
+
 class HttpOfferPay {
   constructor(url) {
     this.url = url;
   }
 
   pay(guid, amount) {
-    return new Promise(resolve => resolve());
+    return new Promise((resolve, reject) => {
+      if (validator.isUUID(guid, 4)) {
+        resolve(guid);
+      } else {
+        reject('Invalid GUID');
+      }
+    })
+      .then((valid) => new Promise(resolve => resolve()));
   }
 }
 export default HttpOfferPay;
