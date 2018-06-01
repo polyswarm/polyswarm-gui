db3e4fa132308f6fee441f337b1b92b936e27423
Robert Lathrop <rl@polyswarm.io>
2018-06-01 02:59:06 -0400

    Mention beta.

--- src/components/Welcome/strings.js
+++ src/components/Welcome/strings.js
@@ -1,5 +1,5 @@
 const strings = {
-  welcome: 'Welcome to PolySwarm,',
+  welcome: 'Welcome to the PolySwarm Beta,',
   decentralized: 'The worlds first decentralized marketplace',
   moreInfo:
     'Simply upload files to create your own bounties and then let the microengines on PolySwarm analyze your file.',
