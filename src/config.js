78cc0a06178818142c75116aa39e6f30612fd76a
Robert Lathrop <rl@polyswarm.io>
2018-06-21 16:02:11 -0400

    Use correct token address.

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ const config = {
   host: 'http://localhost:31337',
   websocket_host: 'ws://localhost:31337',
   daemon: 'polyswarmd',
-  token: '0x41622777febf5ac6503bd7260338b855c6ffdaec'
+  token: '0x9e46a38f5daabe8683e10793b06749eef7d733d1'
 };
 export default config;
